abcaf01cffeb6ed36b0f2431dc05787670ef9f18
Lucasr17 <102689151+Lucasr17@users.noreply.github.com>
2024-10-03 10:16:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = 3000;
+//const port = 3000;
 
 // Middleware pour servir les fichiers statiques (HTML, CSS, etc.) dans le dossier "public"
 app.use(express.static(path.join(__dirname, 'public_html')));
