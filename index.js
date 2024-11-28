5690c42f2bceefc3bbb7aadfca87eac5f55d8701
Lucasr17 <102689151+Lucasr17@users.noreply.github.com>
2024-11-28 08:51:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,22 @@ app.post('/update-json', (req, res) => {
     });
 });
 
+// Endpoint pour mettre à jour le fichier JSON
+app.post('/update-json', (req, res) => {
+    const updatedData = req.body;
+    const filePath = path.join(__dirname, 'public_html', 'motsJOSY_A_VALIDER.json');
+
+    fs.writeFile(filePath, JSON.stringify(updatedData, null, 2), (err) => {
+        if (err) {
+            console.error('Erreur lors de la mise à jour du fichier JSON:', err);
+            return res.status(500).json({ error: 'Erreur lors de la mise à jour du fichier JSON' });
+        }
+        res.json({ message: 'Fichier JSON mis à jour avec succès' });
+    });
+});
+
+
+
 // Route de base pour répondre à "/"
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public_html', 'home.html'));
